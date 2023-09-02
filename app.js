9c12270109d87c2f9dbeb49c31854436144a6262
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-09-02 10:35:34 +0200

    Bind to all interfaces

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.get('/start_comfyui', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
