ff10b34e4791ffc6e982a672b8a7cd2931a582f4
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-04-10 18:27:45 +0200

    Addec config.json to determine which apps to display

--- public/script.js
+++ public/script.js
@@ -1,37 +1,48 @@
 $(document).ready(function() {
-        $('#stopA111').click(function() {
-            $.get('/stop_a1111', function(data) {
-                alert(data);
-            });
-        });
+    $.getJSON("/config.json", function(config) {
+        // Initially hide all rows
+        $("tbody tr").hide();
 
-        $('#startA111').click(function() {
-            $.get('/start_a1111', function(data) {
-                alert(data);
-            });
-        });
-
-        $('#stopKohya').click(function() {
-            $.get('/stop_kohya', function(data) {
-                alert(data);
-            });
-        });
-
-        $('#startKohya').click(function() {
-            $.get('/start_kohya', function(data) {
-                alert(data);
-            });
-        });
-
-        $('#stopComfyUI').click(function() {
-            $.get('/stop_comfyui', function(data) {
-                alert(data);
-            });
-        });
-
-        $('#startComfyUI').click(function() {
-            $.get('/start_comfyui', function(data) {
-                alert(data);
-            });
+        // Iterate over the applications in the config
+        config.applications.forEach(function(app) {
+            // Construct the row's ID and show it
+            $("#row" + app).show();
         });
     });
+
+    $('#stopA111').click(function () {
+        $.get('/stop_a1111', function (data) {
+            alert(data);
+        });
+    });
+
+    $('#startA111').click(function () {
+        $.get('/start_a1111', function (data) {
+            alert(data);
+        });
+    });
+
+    $('#stopKohya').click(function () {
+        $.get('/stop_kohya', function (data) {
+            alert(data);
+        });
+    });
+
+    $('#startKohya').click(function () {
+        $.get('/start_kohya', function (data) {
+            alert(data);
+        });
+    });
+
+    $('#stopComfyUI').click(function () {
+        $.get('/stop_comfyui', function (data) {
+            alert(data);
+        });
+    });
+
+    $('#startComfyUI').click(function () {
+        $.get('/start_comfyui', function (data) {
+            alert(data);
+        });
+    });
+});
