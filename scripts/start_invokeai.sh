e5ae0a9128a3ec8fa7f4bbfc8ce4f480a1cc1932
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-07-04 07:46:32 +0200

    Fixed InvokeAI start script

--- scripts/start_invokeai.sh
+++ scripts/start_invokeai.sh
@@ -2,4 +2,4 @@
 export PYTHONUNBUFFERED=1
 cd /workspace/InvokeAI
 source /venvs/invokeai/bin/activate
-nohup invokeai-web > /workspace/logs/invokeai.log 2>&1 &
+nohup invokeai-web --root /workspace/InvokeAI > /workspace/logs/invokeai.log 2>&1 &
